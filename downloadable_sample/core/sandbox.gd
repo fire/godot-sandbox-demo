98def9ef6455a2854080f57a005b113c17d8e266
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-08-24 09:56:58 -0700

    Save

--- downloadable_sample/core/sandbox.gd
+++ downloadable_sample/core/sandbox.gd
@@ -3,11 +3,11 @@ extends Sandbox
 func _ready() -> void:
 	var buffer
 	buffer = Sandbox.download_program("hello_world")
-	var fa = FileAccess.open("res://downloadable_sample/hello_world.elf", FileAccess.WRITE)
-	fa.store_buffer(buffer)
-	fa.close()
+	#var fa = FileAccess.open("res://downloadable_sample/hello_world.elf", FileAccess.WRITE)
+	#fa.store_buffer(buffer)
+	#fa.close()
 
 	load_buffer(buffer)
 	print("Program buffer size: %d" % buffer.size())
 	print(vmcall("hello_world"))
-	print(vmcall("fibonacci", 4))
+	print(vmcall("fibonacci", 7))
--- slide_knight/core/label.gd
+++ slide_knight/core/label.gd
@@ -1,16 +1,18 @@
 extends Label
 
-var player_sandbox : Sandbox
+@onready
+var player_node = get_node("%Player")
 
 func _ready() -> void:
-	var player = get_node("%Player")
+	var player_sandbox = player_node.get_script().get_sandbox_for(player_node)
 	# Accessing a shared sandbox requires using get_sandbox_for(), which takes a Node instance
-	player_sandbox = player.get_script().get_sandbox_for(player)
+	player_sandbox.add_allowed_object(player_node)
 	# Now we can access the Sandbox used by Player
-	self.text = player_sandbox.get("player_name")
+	self.text = player_node.get("player_name")
 
 func _process(_delta: float) -> void:
 	if self.name == "CallLabel":
+		var player_sandbox = player_node.get_script().get_sandbox_for(player_node)
 		# Get any sandbox, and ask for the global number of VM calls made
 		var total_calls = player_sandbox.get("global_calls_made")
 		self.text = "Virtual Machine\nfunction calls: " + str(total_calls)
